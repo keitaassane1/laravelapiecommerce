6d52dcd13f58b0f59c27b06f1b1d108dd0d60467
keita <keitaassane1@gmail.com>
2017-11-19 16:08:25 +0000

    seeding Database for Product & Review

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,16 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
+
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'customer' =>   $faker->name,
+        'review'	=>  $faker->paragraph,
+        'star'	  =>	$faker->numberbetween(0,5),
+		'product_id' => function(){
+			return	Product::all()->random();	
+		},         
     ];
 });
